2a0cf3891a50183b40022ca0e8833d82b23ba5ec
sreepn <sreepn@deloitte.com>
2020-03-10 13:11:08 +0530

    cleaning up unwanted code

--- src/main/java/spring_assessment/spring_boot_app/HelloWorldController.java
+++ src/main/java/spring_assessment/spring_boot_app/HelloWorldController.java
@@ -3,11 +3,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-/**
- *
- * @author manoj.bardhan
- *
- */
+
 @Controller
 @EnableAutoConfiguration
 public class HelloWorldController {
